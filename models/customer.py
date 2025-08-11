a769be0345810cb8c697401bef74e2f8e3f804fc
zahinmahmud <66882223+zahinmahmud@users.noreply.github.com>
2025-08-11 11:41:25 +0600

    Update customer.py

--- models/customer.py
+++ models/customer.py
@@ -1,1 +1,16 @@
+from  odoo  import models , fields, api
+
+class Customer(models.Model):
+    _name= "finale.customer"
+    _description="Finale Custom Model"
+
+    name = fields.Char(string="Name", require=True)
+    email = fields.Char(string="Email", require=True)
+    address = fields.Char(string="address", require=True)
+    phonenumber=fields.Char(string="Phone_Number", required=True)
+    gender = fields.Selection([('male','Male'),('female','Female')],string="Gender")
+    date = fields.Datetime(string ="Date and time")
+    image = fields.Image(string="Image")
+
+
 
