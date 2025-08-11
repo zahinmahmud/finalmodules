2bc6dfd85fa1ee2fc84f33a93b3b5068b9b6e9cb
zahinmahmud <66882223+zahinmahmud@users.noreply.github.com>
2025-08-11 11:38:11 +0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,23 @@
-
+{
+    "name":"Final module",
+    "description":'Finale modules with all Function',
+    "sequence":-100,
+    "summary":'Finale Modules',
+    "data":[
+        'views/menu.xml',
+        'views/customer.xml',
+        'views/product.xml',
+        'views/inventory.xml',
+        'views/website_templates.xml',  
+        'views/product_checkout.xml',
+        'views/customlayout.xml',
+        'views/homepage.xml',
+        'views/checkout.xml'
+    ],
+    "depends": ["sale", "stock", "mail","web","website"],
+    "installable":True,
+    "application":True,
+    "author":"zahin",
+    'category': 'Inventory sales ',
+    
+}
